89e1eb0103d0294c767127bf48bb1d33981f3def
SURABHI S <63139488+Surbi-22@users.noreply.github.com>
2020-10-18 11:48:07 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,34 @@
+import smtplib
+
+# STMP -Simple Mail Transfer Protocol
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+frm_addr = 'your_id@your_domain.com'
+to_addr  = ['abc@Your_domain.com' , 'xyz@your_domain.com']
+
+msg = MIMEMultipart()
+
+# add 'From' and 'To' headers and subject
+msg['From']    = frm_addr
+msg['To']      = ",".join(to_addr)
+msg['subject'] = 'just to check'
+
+body = 'Hi guys'
+
+#attach message
+msg.attach(MIMEText(body,'plain'))
+
+email    = 'your_id@your_domain.com'
+password = 'password'
+
+mail = smtplib.SMTP('smtp.gmail.com',587)
+mail.ehlo()                #to identify yourself to ESMTP SERVER
+mail.starttls()            # put STMP connection in TLS mode
+mail.login(email,password) #log in on STMP server that require authentication
+
+# message as string
+text = msg.as_string()
+mail.sendmail(frm_addr,to_addr,text)  #send mail
+mail.quit()         #terminate SMTP session
+
